1c1f4217835f2a662904a4ea24e3f8e6be89edf2
ymii <yusukemii2@gmail.com>
2023-06-16 14:51:52 +0900

    Add files via upload

---
+++ operator_2.py
@@ -0,0 +1,30 @@
+a = 5
+b = 3
+
+# 加算のa = a + bと同じ
+a += b
+print(a)
+
+# 減算のa = a - bと同じ
+a -= b
+print(a)
+
+# 乗算のa = a * bと同じ
+a *= b
+print(a)
+
+# 徐算のa = a / bと同じ
+a /= b
+print(a)
+
+# 剰余のa = a % bと同じ
+a %= b
+print(a)
+
+# 自乗のa = a ** bと同じ
+a **= b
+print(a)
+
+# 切り捨て徐算のa = a // bと同じ
+a //= b
+print(a)
