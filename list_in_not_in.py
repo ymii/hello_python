61fe61b6cf5b58e2e5efdcf739861b79b9e383a0
ymii <yusukemii2@gmail.com>
2023-06-16 14:55:19 +0900

    Add files via upload

---
+++ list_in_not_in.py
@@ -0,0 +1,18 @@
+# ３つの要素を含むリストを定義
+fruitList = ["apple", "orange", "banana"]
+
+# 演算子inを使用して、リストfruitListに"orange"が含まれるかを評価する。
+# 含まれる場合はTrueを返す
+print("orange" in fruitList)
+
+# 演算子inを使用して、リストfruitListに"mango"が含まれるかを評価する。
+# 含まれない場合はFalseを返す
+print("mango" in fruitList)
+
+# 演算子not inを使用して、リストfruitListに"grape"が含まれていないかを評価する。
+# 含まれていない場合はTrueを返す
+print("grape" not in fruitList)
+
+# 演算子not inを使用して、リストfruitListに"apple"が含まれていないかを評価する。
+# 含まれる場合はFalseを返す
+print("apple" not in fruitList)
