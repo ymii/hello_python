f06ebb0deec53a03acf12beed83b138cf3b709b6
ymii <yusukemii2@gmail.com>
2023-06-16 14:33:15 +0900

    Add files via upload

---
+++ variable.py
@@ -0,0 +1,33 @@
+# 変数xを定義して値"John"を代入。
+# print関数でxの値を画面表示
+x = "John"
+print(x)
+
+# 変数yを定義して値30を代入。
+# print関数でyの値を画面表示
+y = 30
+print(y)
+
+# 変数zを定義して変数xを代入。
+# print関数でzの値を画面表示
+z = x
+print(z)
+
+# 変数wを定義して変数yを代入。
+# print関数でwの値を画面表示
+w = y
+print(w)
+
+# 複数の変数を同時に定義して代入。
+# print関数で各変数値を表示
+a, b, c = "cat", "cog", "horse"
+print(a)
+print(b)
+print(c)
+
+# 複数の変数に一つの値を代入。
+# print関数で各変数を表示
+d = e = f = "car"
+print(d)
+print(e)
+print(f)
