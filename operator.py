21f05a332ec9d187dd91f0c2854a515a2d72fb31
ymii <yusukemii2@gmail.com>
2023-06-16 14:49:25 +0900

    Add files via upload

---
+++ operator.py
@@ -0,0 +1,30 @@
+a = 5
+b = 2
+
+# 加算 +
+c = a + b
+print(c)
+
+# 減算 -
+c = a - b
+print(c)
+
+# 乗算 *
+c = a * b
+print(c)
+
+# 徐算 /
+c = a / b
+print(c)
+
+# 剰余 %
+c = a % b
+print(c)
+
+# 自乗 **
+c = a ** b
+print(c)
+
+# 切り捨て徐算 //
+c = a // b
+print(c)
