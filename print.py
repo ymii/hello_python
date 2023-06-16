873f5337caac33b2ebd96307a9ca183001585739
ymii <yusukemii2@gmail.com>
2023-06-16 14:21:56 +0900

    Add files via upload

---
+++ print.py
@@ -0,0 +1,16 @@
+# 画面に"Hello, World"と表示
+print("Hello, World!")
+
+# 画面に"I love python"と表示
+print("I love python")
+
+# カッコ内に直接計算式を入力して画面に計算結果を表示。
+# 計算式は""で囲まないこと
+print(1 + 5)
+
+# 計算式10 x 5の計算結果を表示
+print(10 * 5)
+
+# 変数nameに"John Smith"を代入して表示
+name = "John Smith"
+print(name)
